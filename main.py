0cddfeefb56c7f28a307428eb19798a9698fbea6
RSandyBoi <Ryans121@outlook.com>
2025-11-18 00:15:20 -0800

    added fps limit, player class, circleshape class, working on drawing

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0 # Limit to 60 FPS
+        
 
 
 
